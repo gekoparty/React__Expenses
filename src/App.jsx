a6866910ca5f5b60a14281664ad4a3bdc53868c9
gekoparty <gekoparty@hotmail.com>
2021-09-15 18:21:43 +0200

    Rendering List and adding key

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-const App = () => {
-  const expenses = [
-    {
+const DUMMY_EXPENSES = [
+  {
       id: "e1",
       title: "Car Insurance",
       amount: 294.4,
@@ -27,11 +27,18 @@ const App = () => {
       amount: 128,
       date: new Date(2020, 8, 17),
     },
-  ];
+];
+
+
+
+const App = () => {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense)
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
   };
 
   return (
